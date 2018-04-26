e630612c12f743c15ea55aca7093d719200edfe8
James <DarkHeart527@users.noreply.github.com>
2018-04-26 08:50:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,106 +6,127 @@ let app = {
 			source: 'dontplay_preview.mp3',
 			description: 'This was made at the top of my head, fire, yet simple. Note that this is only a preview (or teaser).',
 			releaseDate: '4 - 24 - 2018',
+			fireRating: 'Not rated',
 		},
 		'Melted Mixtape': {
 			source: 'Melted Mixtape.mp3',
 			description: 'This is fire, I made this in only ONE day, actually a few hours, the synths were made by accident (made it but not intended to sound like it does) and just went with it.',
 			releaseDate: '3 - 10 - 2018',
+			fireRating: 5,
 		},
 		'121': {
 			source: '121.mp3',
 			releaseDate: 'Unknown',
 			description: '',
+			fireRating: 3,
 		},
 		'Another': {
 			source: 'Another.mp3',
 			releaseDate: '',
 			description: '',
+			fireRating: 1,
 		},
 		'Celeste': {
 			source: 'Celeste.mp3',
 			releaseDate: '1 - 7 - 2018',
 			description: '',
+			fireRating: 4,
 		},
 		'Chaotic': {
 			source: 'Chaotic.mp3',
 			releaseDate: '2 - 7 - 2018',
 			description: '',
+			fireRating: 3,
 		},
 		'Chill': {
 			source: 'Chill.mp3',
 			releaseDate: '',
 			description: '',
+			fireRating: 2,
 		},
 		'Death': {
 			source: 'Death.mp3',
 			releaseDate: '',
 			description: '',
+			fireRating: 3,
 		},
 		'E.Z. Lop Les': {
 			source: 'E.Z. Lop Les.mp3',
 			releaseDate: '',
 			description: '',
+			fireRating: 4,
 		},
 		'Finally': {
 			source: 'Finally.mp3',
 			releaseDate: '',
 			description: '',
+			fireRating: 2,
 		},
 		'Flames': {
 			source: 'Flames.mp3',
 			releaseDate: '',
 			description: '',
+			fireRating: 3,
 		},
 		'Flute': {
 			source: 'Flute.mp3',
 			releaseDate: '',
 			description: '',
+			fireRating: 1,
 		},
 		'In Your Head': {
 			source: 'In your head.mp3',
 			releaseDate: '2 - 9 - 2018',
 			description: '',
+			fireRating: 3,
 		},
 		'Life Is Life': {
 			source: 'Life Is Life.mp3',
 			releaseDate: '',
 			description: '',
+			fireRating: 1,
 		},
 		'Lil Happy': {
 			source: 'Lil Happy.mp3',
 			releaseDate: '',
 			description: '',
+			fireRating: 2,
 		},
 		'Mass': {
 			source: 'Mass.mp3',
 			releaseDate: '',
 			description: '',
+			fireRating: 2,
 		},
 		'Money': {
 			source: 'Money.mp3',
 			releaseDate: '12 - 15 - 2018',
 			description: '',
+			fireRating: 3,
 		},
 		'Nearly Nothing': {
 			source: 'Nearly Nothing.mp3',
 			releaseDate: '',
 			description: '',
+			fireRating: 2,
 		},
 		'Too Late': {
 			source: 'Too Late.mp3',
 			releaseDate: '',
 			description: '',
+			fireRating: 3,
 		},
 		'Trash': {
 			source: 'Trash.mp3',
 			releaseDate: '',
 			description: '',
+			fireRating: 1,
 		},
 		'Yet Simple': {
 			source: 'Yet Simple.mp3',
 			releaseDate: '',
 			description: '',
+			fireRating: 2,
 		},
 	},
 	path: 'tracks/',
@@ -209,6 +230,8 @@ window.onload = function() { // Make sure to start the program AFTER the page lo
 				searchTerm = app.tracks[tracksEx[j].innerHTML].releaseDate.toUpperCase().indexOf(val);
 			} else if (document.getElementById('searchBy').value == 'featured') {
 				//searchTerm = app.tracks[tracksEx[j].innerHTML].releaseDate.toUpperCase().indexOf(val);
+			} else if (document.getElementById('searchBy').value == 'fireRating') {
+				searchTerm = app.tracks[tracksEx[j].innerHTML].fireRating.toUpperCase().indexOf(val);
 			}
 			if (searchTerm > -1) {
 				tracksEx[j].style.display = 'block';

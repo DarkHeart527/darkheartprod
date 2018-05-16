7cbbb3d567ed500880710d8d10a50492d25e8371
James <DarkHeart527@users.noreply.github.com>
2018-05-15 21:00:47 -0700

    Add files via upload

--- appiframe.js
+++ appiframe.js
@@ -1,5 +1,5 @@
 (function() {
-console.info(`Hello! I see that you are peeking through the console... and the code. Unfortunatly, that is NOT how you get the beats. Anyway, since you're here, you should talk to me about collaborating with me in music production or website development. \nContact me at darkheartprod@gmail.com\n `);
+console.info(`Music By DarkHeart527`);
 
 let app = {
 	tracks: { /* Please note that tracks that start with a number must be at the top */
@@ -149,9 +149,6 @@ let app = {
 		},
 	},
 	path: '%74%72%61%63ks/',
-	easterEgg: {
-		
-	},
 	openFile: function(file) {
 		console.log(`Opening the file ${file}\n `);
 		/* Set up a progress bar (Would love a canvas progress bar [nice looking!]) */
@@ -159,55 +156,23 @@ let app = {
 		this.audio.src = this.path.toLowerCase() + file;
 		console.log(unescape(this.path.toLowerCase()));
 		this.audio.onloadedmetadata = function() {
-			let pBar = document.getElementById('progress');
-			let time = document.getElementById('time');
-			this.pBarInt = setInterval( function() {
-				let cTime = Math.floor(app.audio.currentTime); /* Remove Fractions */
-				let minute = 0;
-				let second = cTime;
-				for (let i = 0; i < Math.floor(second)/60 - 1; i++) {
-					minute++;
-					second -= 60;
-				}
-				if (second < 10) {
-					second = '0' + second;
-				}
-				time.innerHTML = `${minute}:${second}`;
-				
-				pBar.style.width = (app.audio.currentTime / app.audio.duration) * 100 + '%';
-			}, 50);
 			this.play();
 		}
-		document.getElementById('playBtn').innerHTML = 'Pause';
 	},
 	currentTrack: 0,
 	pBarInt: '',
 	started: false,
 	play: function(cPX) {
 		if (this.started) {
-		let msg;
+		let img;
 			if (this.audio.paused) {
 				this.audio.play();
-				msg = 'Pause';
+				img = 'Pause';
 			} else {
 				this.audio.pause();
-				msg = 'Play';
+				img = 'Play';
 			}
-			return msg;
-		} else {
-			this.cPX++;
-			msg = '<center style="font-size: ' + this.cPX + 'px;">Select a track first</center>';
-			if (app.iframe != true) {
-				if (this.cPX == 24) {
-					/* Click the text when you first start 10 times without selecting a song */
-					app.easterEgg.One = document.createElement('link');
-					app.easterEgg.One.href = 'easterEggs/One.css';
-					app.easterEgg.One.type = 'text/css';
-					app.easterEgg.One.rel = 'stylesheet';
-					document.head.appendChild(app.easterEgg.One);
-				}
-				return msg;
-			}
+			return img;
 		}
 	},
 	iframe: false,
@@ -253,16 +218,13 @@ let app = {
 		let trackRelease = obs.releaseDate;
 		let trackDesc = obs.description;
 		app.openFile(obs.source);
-		if (app.iframe != true) {
-			document.getElementById('trackTitle').innerHTML = trackName;
-			document.getElementById('releaseDate').innerHTML = 'Release: ' + trackRelease;
-			document.getElementById('description').innerHTML = trackDesc;
-		}
 	},
 	audio: '',
 	init: function() {
 		this.audio = new Audio();
 		this.audio.autoPlay = true;
+		this.setupTrack();
+		this.audio.play();
 		this.audio.onended = function() {
 			app.nextTrack();
 		}
@@ -271,71 +233,6 @@ let app = {
 
 
 window.onload = function() { /* Make sure to start the program AFTER the page loads */
-	if (app.iframe != true) {
-		for (let i = 0; i < app.tracks.length(); i++) {
-			let trackName = app.tracks.keys()[i];
-			let track = app.tracks[trackName];
-			let source = track.source;
-			let trackDesc = track.description;
-			let trackRelease = track.releaseDate;
-			
-			let trackEl = document.createElement('div');
-			trackEl.className = 'track';
-			trackEl.innerHTML = trackName;
-			trackEl.onclick = function() {
-				app.openFile(source);
-				app.started = true;
-				app.currentTrack = i;
-				document.getElementById('trackTitle').innerHTML = trackName;
-				document.getElementById('releaseDate').innerHTML = 'Release: ' + trackRelease;
-				document.getElementById('description').innerHTML = trackDesc;
-			}
-			document.getElementsByClassName('trackList')[0].appendChild(trackEl);
-		}
-		let searchInput = document.getElementById('searchTracks');
-		searchInput.onkeyup = function() {
-			var searchTerm, val, tracksEx, found;
-			found = 0;
-			val = this.value.toUpperCase();
-			tracksEx = document.getElementsByClassName('track');
-			for (let j = 0; j < tracksEx.length; j++) {
-				if (document.getElementById('searchBy').value == 'name') {
-					searchTerm = tracksEx[j].innerHTML.toUpperCase().indexOf(val);
-				} else if (document.getElementById('searchBy').value == 'release') {
-					searchTerm = app.tracks[tracksEx[j].innerHTML].releaseDate.toUpperCase().indexOf(val);
-				} else if (document.getElementById('searchBy').value == 'featured') {
-					/* searchTerm = app.tracks[tracksEx[j].innerHTML].releaseDate.toUpperCase().indexOf(val); */
-				} else if (document.getElementById('searchBy').value == 'fireRating') {
-					searchTerm = app.tracks[tracksEx[j].innerHTML].fireRating.toUpperCase().indexOf(val);
-				}
-				if (searchTerm > -1) {
-					tracksEx[j].style.display = 'block';
-					document.getElementById('noResults').style.display = 'none';
-					found++;
-				} else {
-					tracksEx[j].style.display = 'none';
-					if (found <= 0) {
-						document.getElementById('noResults').style.display = 'block';
-					}
-				}
-			}
-		}
-		document.getElementById('searchBy').onchange = function() {
-			searchInput.onkeyup(); /* Search Again When The Filter Is Changed */
-		}
-		
-		document.getElementById('playBtn').onclick = function() {
-			this.innerHTML = app.play();
-		}
-		
-		document.getElementById('nextBtn').onclick = function() {
-			app.nextTrack();
-		}
-		
-		document.getElementById('previousBtn').onclick = function() {
-			app.previousTrack();
-		}
-	}
 	app.init();
 	// Link Handling
 	let lHHref = location.href;
@@ -349,13 +246,10 @@ window.onload = function() { /* Make sure to start the program AFTER the page lo
 			console.log(lHValue)
 			console.log(`Command ${lHCommand} recieved with the value of ${lHValue}\n `);
 			switch(lHCommand) {
-				case 'open':
-					app.openFile(lHValue);
+				case 'playlist':
+				
 				break;
-				case 'redopen':
-					window.open(lHValue);
-				break;
-				default: 
+				default:
 					console.log(`Please note that the command ${lHCommand} was not found\n `);
 				break;
 			}

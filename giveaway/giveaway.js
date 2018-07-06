07508e7828fd7b04f594abffe67ebcfc31e43bc1
James <DarkHeart527@users.noreply.github.com>
2018-07-06 15:52:21 -0700

    Add files via upload

---
+++ giveaway/giveaway.js
@@ -0,0 +1,505 @@
+let people = {
+	"boneztherealest": 1,
+	"jkidbnn": 1,
+	"pankaj61284": 1,
+	"_memmedzade.o1": 1,
+	"veysel.sayy": 1,
+	"khtbjdw": 1,
+	"n129z": 1,
+	"dil_ki_duniya__": 1,
+	"butk_gym": 1,
+	"zafarshah1540": 1,
+	"_aashika_fan_club": 1,
+	"lariiimariano": 1,
+	"m.u.r.a_d": 1,
+	"woualiii": 1,
+	"mustafa_hnyl_1905": 1,
+	"akyildiz_5634": 1,
+	"uzmanaskerler2": 1,
+	"shaye8191": 1,
+	"aravind_nani123": 1,
+	"i_am_oviad": 1,
+	"ulasstunaaastepe": 1,
+	"enni_bey2": 1,
+	"o_ses_sensin_izmir": 1,
+	"saglikciabi": 1,
+	"agustriadi0312": 1,
+	"bikekagram": 1,
+	"punjabi_singers_life_style": 1,
+	"emosla_fal": 1,
+	"m.abdurrahman": 1,
+	"kevinsinh_gohil": 1,
+	"abrar_king_khan_": 1,
+	"emir._.ates": 1,
+	"cagann_bulutt": 1,
+	"istek_at_bekle_mq": 1,
+	"556556556ibra": 1,
+	"sayyed_iram_5939": 1,
+	"cetin5858ahmet": 1,
+	"life4vess": 1,
+	"ismimiz_yokkkk": 1,
+	"rizky.ynp": 1,
+	"sudeenas1": 1,
+	"__keen1__": 1,
+	"76568hyrfgh": 1,
+	"emrekullebey": 1,
+	"gill17_23": 1,
+	"ece_keskin.0": 1,
+	"marasukal": 1,
+	"w.o.w_kimsohyun72": 1,
+	"rizkiputra_c": 1,
+	"pesrak_rawani": 1,
+	"ycyoxtxotz": 1,
+	"sutisnad066": 1,
+	"suhailkhan1245": 1,
+	"theyh8onshyaa": 1,
+	"happydami_": 1,
+	"ifresh.indonesia": 1,
+	"kits99900": 1,
+	"nathparkash8": 1,
+	"handepriw": 1,
+	"naman97530": 1,
+	"zaidahmed1786": 1,
+	"trusted_men": 1,
+	"ahmetdagan80": 1,
+	"kannan890": 1,
+	"kadirsahin_1903.55": 1,
+	"sam_saner": 1,
+	"daniel_store03": 1,
+	"meshmsht": 1,
+	"anjalisharma970": 1,
+	"kklf_8564": 1,
+	"davidmanly_16": 1,
+	"being_techno": 1,
+	"foto_edi_zp": 1,
+	"ejv_3": 1,
+	"imprakashkrsingh": 1,
+	"indri_ria63": 1,
+	"bibeneksiktm": 1,
+	"yeeldaayy": 1,
+	"hrithikroshan_fg": 1,
+	"262c3962": 1,
+	"gamer.highlight.s": 1,
+	"deniz_gulben_fan": 1,
+	"angelrodriguezspam": 1,
+	"segafredo18": 1,
+	"surajkadam0649": 1,
+	"inb.official": 1,
+	"naved_600": 1,
+	"muhammed.sakc": 1,
+	"arshsvnt07": 1,
+	"notyy_b0y_": 1,
+	"kamal_x.ov": 1,
+	"vitry.kristiani": 1,
+	"kadir.krbnq": 1,
+	"shadowfight_picss": 1,
+	"davidrasti_": 1,
+	"iliyas_ali_sayyed99": 1,
+	"innocent_khan_0786": 1,
+	"__jannatzubair_fans__": 1,
+	"zaif1137": 1,
+	"red_bollywood": 1,
+	"__saglam_yasam__": 1,
+	"hasan__demirkran": 1,
+	"mr_akash_96": 1,
+	"chrisportillo122": 1,
+	"risalrony": 1,
+	"ataccansu": 1,
+	"mr_romantic_7912": 1,
+	"scd_xox": 1,
+	"riks952": 1,
+	"dj_ooo1": 1,
+	"jadeja4144": 1,
+	"pendek.a": 1,
+	"kzjzhsgsjqhq": 1,
+	"worldcup18x": 1,
+	"ostemirov345": 1,
+	"patilpatil80": 1,
+	"khairesanju": 1,
+	"o_ofm1": 1,
+	"nossyy12": 1,
+	"elbillysirg": 1,
+	"vipulrai6": 1,
+	"amit1191998": 1,
+	"ayaj_42": 1,
+	"great.maskspirulina": 1,
+	"guru_randhawa_islife": 1,
+	"saliciathompson": 1,
+	"putrimr29": 1,
+	"orjinalkosmetik": 1,
+	"___cutiee____pie___": 1,
+	"cancancan3333": 1,
+	"dogx_lover": 1,
+	"sunbi119": 1,
+	"cancanfoni": 1,
+	"krish.mittal3": 1,
+	"sunny.010": 1,
+	"x.husom.x": 1,
+	"jjgyhfdafyjf": 1,
+	"real_kangdaniel": 1,
+	"heso638": 1,
+	"pubgmoblturkye": 1,
+	"sekil_veririm_kimene": 1,
+	"san_study68": 1,
+	"zaynmalik1403": 1,
+	"taner761": 1,
+	"mejabelajar015": 1,
+	"helen_murphy14": 1,
+	"alidemirr.demirr": 1,
+	"melisa.smsk0": 1,
+	"lz.mm": 1,
+	"ecelik6": 1,
+	"djan_nabiagali": 1,
+	"nfl_fal07": 1,
+	"endless__jatt__mehkma": 1,
+	"bothesapp1986": 1,
+	"gamersruzgar4674": 1,
+	"fucking_luis__": 1,
+	"unisa5703": 1,
+	"sisska_aul": 1,
+	"mamiaga4715": 1,
+	"arakuy99": 1,
+	"aashikkhan970": 1,
+	"sarita12az": 1,
+	"un_kown_0003": 1,
+	"lfs_modlari": 1,
+	"wizkhalifa_here": 1,
+	"paravellimahesh": 1,
+	"xaman.sing": 1,
+	"cllonis": 1,
+	"mr_xtupid": 1,
+	"anurag_bhuria": 1,
+	"nik_rathod009": 1,
+	"adeputra228": 1,
+	"nemusis__vediet": 1,
+	"hojiakbar1425": 1,
+	"mervexx.11": 1,
+	"rojingencdal": 1,
+	"riyaz_mittur": 1,
+	"vihu_srivastava": 1,
+	"amri_fahmy": 1,
+	"jo_oj_4": 1,
+	"sevdalim_6005": 1,
+	"chahatmhjn_": 1,
+	"_borny__nawab_00": 1,
+	"hendrawansaputra350": 1,
+	"llllllll6_": 1,
+	"mr_radha_": 1,
+	"itjdn": 1,
+	"bikeshbasnet": 1,
+	"anmolmehrwal56": 1,
+	"girraj_agarwal": 1,
+	"ngaiawolfie": 1,
+	"malkahmed_1": 1,
+	"nfksox": 1,
+	"dakchil": 1,
+	"anky_raaj": 1,
+	"dazzle.me.love": 1,
+	"missleylamemmedova": 1,
+	"indian850": 1,
+	"muh_apin17": 1,
+	"sajadzadenajaf8": 1,
+	"powerfroster": 1,
+	"marobhai11": 1,
+	"burcusantos": 1,
+	"tas_kokulu_tasarm": 1,
+	"babygirlislovely.14": 1,
+	"samarhassanofficial": 1,
+	"prajwal_chavan26363": 1,
+	"prathmeshdigrse": 1,
+	"ma_hope_07": 1,
+	"ozlemim.sna": 1,
+	"amantiwari2019": 1,
+	"ndng_batuhan1": 1,
+	"nuri8804_": 1,
+	"yasminajudy": 1,
+	"sefaa_yilmazz01": 1,
+	"ahmeed._haassaan": 1,
+	"wryinb": 1,
+	"amit.patel.9256": 1,
+	"shubam4145": 1,
+	"errehaan88": 1,
+	"new_view_on_art": 1,
+	"itsdasungeunida": 1,
+	"vishalyadav220399": 1,
+	"x_ibociginiz_xd": 1,
+	"indian_legends5": 1,
+	"x_faur_x": 1,
+	"farak_koraeh_": 1,
+	"ziahasnainofficial61": 1,
+	"choudharyshaab222": 1,
+	"ad_3.5_": 1,
+	"snanea_usp": 1,
+	"jabal_mohsen0": 1,
+	"aa9789182": 1,
+	"aaaaaf123456": 1,
+	"_.thehalfbloodprince._": 1,
+	"_its_devu_": 1,
+	"m._soydan": 1,
+	"saidfe5": 1,
+	"atul_thongire": 1,
+	"hribhav": 1,
+	"safakcandas1903": 1,
+	"hamze521j": 1,
+	"aliko.rap": 1,
+	"mirza__ali": 1,
+	"aditya_agusmayufi": 1,
+	"rangga_fadli": 1,
+	"frknyasrofficial": 1,
+	"seyid_berdeli_": 1,
+	"iamsanjayseth": 1,
+	"aaaaaaa_hshhdgd": 1,
+	"_geeta1.2": 1,
+	"jritok": 1,
+	"zehra_5123": 1,
+	"auto_certobr": 1,
+	"_arthur21_": 1,
+	"selena_netoo": 1,
+	"silvany_bittencourt": 1,
+	"paeseduu": 1,
+	"luiz_2017fernando": 1,
+	"ta_tudo_liberad0": 1,
+	"boonkganvgofficial": 1,
+	"komilkhan990": 1,
+	"yuceiaksoy": 1,
+	"tugbaustn1234": 1,
+	"bornali8253": 1,
+	"alechujjaja": 1,
+	"michelale82": 1,
+	"erdem_ilkay_mey_duduk": 1,
+	"mamaedhenga": 1,
+	"domiinika90": 1,
+	"mmikelouis004": 1,
+	"mmikelouis002": 1,
+	"itsroseslife_": 1,
+	"ko_maestro": 1,
+	"dummybuddy2018": 1,
+	"rtytip2223": 1,
+	"yana_cheese_top": 1,
+	"ha.la3843": 1,
+	"ikonikofficial": 1,
+	"roshan_h_h": 1,
+	"dmusic.07": 1,
+	"loosaz07": 1,
+	"kysudirtyhoe": 1,
+	"countess_ravan": 1,
+	"_nzaz": 1,
+	"yter90": 1,
+	"of.ezequielpedrozo": 1,
+	"vigsatmonitoramento": 1,
+	"higienopolis_sp": 1,
+	"facacriolla_cutelariaelmo": 1,
+	"nata._._.999": 1,
+	"mundo_e_viagens": 1,
+	"diogorelikia": 1,
+	"evssilvabarros": 1,
+	"_lolooo97_": 1,
+	"lorranemelo623": 1,
+	"jasaniiihoee": 1,
+	"uhsusxn09099": 1,
+	"leomattoskl": 1,
+	"34minute": 1,
+	"milansingh2": 1,
+	"muagessiaa": 1,
+	"focaspaulo": 1,
+	"ggg52100": 1,
+	"dipsidarzo": 1,
+	"epg00845": 1,
+	"ronaldogeigelosan": 1,
+	"x.usama.x": 1,
+	"bearmantag": 1,
+	"klicfast": 1,
+	"vjollca906": 1,
+	"liphe2132018": 1,
+	"brown_skinjayyy": 1,
+	"vip.samm.253": 1,
+	"detreon.beats": 1,
+	"meydiocre": 1,
+	"_.r.i.p": 1,
+	"blxck14": 1,
+	"itsmdk2020": 1,
+	"jo._.lynn": 1,
+	"lil_daflipdgod2": 1,
+	"parcrew_": 1,
+	"skip_ness_pk": 1,
+	"hayastan47": 1,
+	"talhakhan3322": 1,
+	"guilmiyassine": 1,
+	"botcashtrafficmoney": 1,
+	"biggerinfinity2014": 1,
+	"mingkwanmickey": 1,
+	"mixtokn": 1,
+	"sexykinl": 1,
+	"eckhartwjp253": 1,
+	"jenmarie92301": 1,
+	"princessa__vanessa__": 1,
+	"_datboii_quan": 1,
+	"nelsontaylor284": 1,
+	"rose_wolf321": 1,
+	"girlthatskeithh": 1,
+	"hearteyeslover": 1,
+	"marvin_gay16": 1,
+	"dejakizzee": 1,
+	"cade.kefren": 1,
+	"lucasmattos.30": 1,
+	"brobrocheche7": 1,
+	"pinelopiandreadi": 1,
+	"booyoutoo2": 1,
+	"darshpoddar": 1,
+	"darinkamatsho": 1,
+	"_300_jessie": 1,
+	"danini05": 1,
+	"bakirkoydenim": 1,
+	"niator7453": 1,
+	"amaryliss162018": 1,
+	"khac.thanh.9": 1,
+	"lincecaiu": 1,
+	"sahmanrohadi63": 1,
+	"cashkiosk": 1,
+	"dessrhyme": 1,
+	"jove190": 1,
+	"niator75": 1,
+	"queralesangelo": 1,
+	"carlostorres6748": 1,
+	"alexanderberzunza": 1,
+	"iliev7878": 1,
+	"frannbvcx": 1,
+	"_alexzamora_": 1,
+	"luizfelipesilvamg": 1,
+	"goddenzzzz": 1,
+	"kevinx89x": 1,
+	"thekorn103": 1,
+	"polonicosa": 1,
+	"lara.abashova.83": 1,
+	"miguel_lopez_200": 1,
+	"_what_the_fu_ck": 1,
+	"lilleancup": 1,
+	"nene_your_girl": 1,
+	"nolimit_jimmy": 1,
+	"elijah.k.a": 1,
+	"federico.posey": 1,
+	"meme_god.21": 1,
+	"tragic.bri": 1,
+	"no.9thomas": 1,
+	"justiincovarrubias": 1,
+	"_just.cedric": 1,
+	"henry_big_sausage": 1,
+	"official_0ohboi": 1,
+	"keanu1583": 1,
+	"2k.skrtskrt": 1,
+	"offadriann": 1,
+	"fernaando.s": 1,
+	"chacoescalanteporras": 1,
+	"nonnie.babyy": 1,
+	"_anthony_bar_": 1,
+	"kenan_ismayilov_11": 1,
+	"kai.anderson.ahs": 1,
+	"darky.freaky": 1,
+	"curlyheadjordain": 1,
+	"such_a_choch": 1,
+	"_aye.itz.viper_": 1,
+	"_stars_darkness_4208": 1,
+	"crxxng_slxt": 1,
+	"5b.ox": 1,
+	"bad_company_parkour": 1,
+	"robbie__bates": 1,
+	"alphalfa69_official": 1,
+	"batmetal_115": 1,}
+// Add up all of the tickets and set peoples chances
+let tickets = 0;
+for (let i = 0; i < Object.keys(people).length; i++) {
+	let name = Object.keys(people)[i];
+	let tix;
+	tix = people[name];
+	let a = people[name] = {};
+	a.tickets = tix;
+	a.chance = 0;
+	tickets += tix;
+}
+for (let j = 0; j < Object.keys(people).length; j++) {
+	let name = Object.keys(people)[j];
+	let obj = people[name];
+	let tix = obj.tickets;
+	let chance = 0;
+	chance =  tix / tickets;
+	obj.chance = chance.toFixed(4);
+}
+// Wait for the page to load and add it to it
+window.onload = function() {
+	// Add search function
+	document.getElementById('search').onkeyup = function() {
+		let val = this.value;
+		if (!people[val] && val != '') {
+			document.getElementById('searchData').innerHTML = 'User Not Found, if you believe that this is an error. Please Message me <i>@darkheart_prod</i> on Instagram.';
+		} else if (people[val]) {
+			let person = people[val];
+			let chance = person.chance;
+			let tix = person.tickets;
+			let tixAmount;
+			if (tix == 1) {
+				tixAmount = '1 ticket';
+			} else {
+				tixAmount = `${tix} tickets`;
+			}
+			document.getElementById('searchData').innerHTML = `<i>${val}</i> has ${tixAmount} and a chance of ${chance}%.`;
+		} else if (!val) {
+			document.getElementById('searchData').innerHTML = '';
+		}
+	}
+	// Make a sign in
+	document.getElementById('signin').onclick = function() {
+		let val = prompt('Password');
+		let decode = function(val) {
+			let decode = new Uint8Array(val);
+			let decoder = new TextDecoder();
+			let decoded = decoder.decode(decode);
+			return decoded;
+		}
+		let code = decode([119, 105, 110, 110, 101, 114, 115, 111, 110, 108, 121])
+		if (val == code) {
+			localStorage.setItem('code', code);
+			localStorage.setItem('signedin', true);
+			location = location;
+		} else {
+			alert('The password is incorrect');
+		}
+	}
+	// Put the ticket display up
+	document.getElementById('tickets').innerHTML = `Tickets: ${tickets}`;
+	// Add the users joined
+	for (let i = 0; i < Object.keys(people).length; i++) {
+		let name = Object.keys(people)[i];
+		let chance = people[name].chance;
+		let tix = people[name].tickets;
+		let div = document.createElement('div');
+		let userName = document.createElement('p');
+		userName.innerHTML = `${name}`;
+		let chances = document.createElement('p');
+		chances.innerHTML = `Chance: ${chance}%`;
+		let ticketsin = document.createElement('p');
+		ticketsin.innerHTML = `Tickets: ${tix}`;
+		div.appendChild(userName);
+		div.appendChild(ticketsin);
+		div.appendChild(chances);
+		document.getElementById('usersJoined').appendChild(div);
+	}
+	(function() {
+		let decode = function(val) {
+			let decode = new Uint8Array(val);
+			let decoder = new TextDecoder();
+			let decoded = decoder.decode(decode);
+			return decoded;
+		}
+		let code = decode([119, 105, 110, 110, 101, 114, 115, 111, 110, 108, 121])
+		let signedin = localStorage.getItem('signedin');
+		let pass = localStorage.getItem('code');
+		if (pass == code) {
+			document.getElementById('signin').innerHTML = 'Sign Out';
+			document.getElementById('signin').onclick = function() {
+				localStorage.clear('signedin');
+				localStorage.clear('code');
+				location = location;
+			}
+		}
+	})();
+}

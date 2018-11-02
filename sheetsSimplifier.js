ee69f4a8a3e611e73b5cd9e0df0c5c87dbbe29cb
James <DarkHeart527@users.noreply.github.com>
2018-11-02 13:31:50 -0700

    Update sheetsSimplifier.js

--- sheetsSimplifier.js
+++ sheetsSimplifier.js
@@ -1,22 +1,8 @@
-
-      // Client ID and API key from the Developer Console
-      var CLIENT_ID = '1059757562983-o8p9bj4l12gkrj5orekusauom3tlien5.apps.googleusercontent.com';
+var CLIENT_ID = '1059757562983-o8p9bj4l12gkrj5orekusauom3tlien5.apps.googleusercontent.com';
       var API_KEY = 'AIzaSyD7A_fA6gtbxXs-sqdeC3oplUVr1zzSZ2U';
-      // Array of API discovery doc URLs for APIs used by the quickstart
       var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
-      // Authorization scopes required by the API; multiple scopes can be
-      // included, separated by spaces.
       var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
-      /**
-       *  On load, called to load the auth2 library and API client library.
-       */
-      function handleClientLoad() {
-        gapi.load('client:auth2', initClient);
-      }
-      /**
-       *  Initializes the API client library and sets up sign-in state
-       *  listeners.
-       */
+
       function initClient() {
         gapi.client.init({
           apiKey: API_KEY,
@@ -25,10 +11,7 @@
           scope: SCOPES
         });
       }
-      /**
-       * Print the names and majors of students in a sample spreadsheet:
-       * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-       */
+
       let Sheets = {
         read: function(range) {
           gapi.client.sheets.spreadsheets.values.get({

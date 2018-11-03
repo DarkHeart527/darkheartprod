a35ed4e323ad8262cce2b0a96f8e6254d6a7242d
James <DarkHeart527@users.noreply.github.com>
2018-11-02 19:17:19 -0700

    Update sheetsSimplifier.js

--- sheetsSimplifier.js
+++ sheetsSimplifier.js
@@ -1,4 +1,4 @@
-document.onload = function() { // UPDATE NOW!
+document.onload = function() { 
 let gscript = document.createElement('script');
 gscript.src = 'https://apis.google.com/js/api.js';
 gscript.async = true;

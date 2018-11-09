83a0b1b477ab33b4056583577ef263fa3c7dc12f
James <DarkHeart527@users.noreply.github.com>
2018-11-08 18:22:44 -0800

    Update sheetsSimplifier.js

--- sheetsSimplifier.js
+++ sheetsSimplifier.js
@@ -57,4 +57,15 @@ var CLIENT_ID = '1059757562983-o8p9bj4l12gkrj5orekusauom3tlien5.apps.googleuserc
            Sheets.result = 'Error ' + response.result.error.message
           });
         },
+        write: function(range, value) {
+                gapi.client.sheets.spreadsheets.values.update({
+                   spreadsheetId: '1y9AiKbLUZ9MiZQZpAtqubu0Eg1iLrhYTjUpIuBmI2k0',
+                   range: 'A3',
+                   valueInputOption: 'USER_ENTERED',
+                   resource: {values:[[value]]}
+                }).then((response) => {
+                  var result = response.result;
+                  console.log(`${result.updatedCells} cells updated.`);
+                });      
+        },
       }

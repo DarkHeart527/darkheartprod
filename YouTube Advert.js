c0686a565e38789a04d8f8b340c22e9d8c40ccc5
James <DarkHeart527@users.noreply.github.com>
2018-11-20 11:09:07 -0800

    Add files via upload

---
+++ YouTube Advert.js
@@ -0,0 +1,151 @@
+let ytads = {
+	videos: ['rjteBAIPnE0'], // Only use ids of the videos, for start time use ',' then seconds (rjteBAIPnE0,35)
+	settings: {
+		length: 30, // How long the video must play, use "full" to set as the entire video
+		autoPlay: false, // No views are counted when true, Views counted when false because the user must press play
+		playType: 'inline', // "random" to select r.andom videos, "inline" to cycle through (1 > 2 > 3)
+		cover: true, // Cover the video with an invisible element upon playing (prevents button pressing)
+		canMute: true, // The video can be muted
+		timeShows: true, // Show the time of the ad
+		timeFormat: '=current= / =duration=', // Choose the time format =current= shows the current time
+		// =duration= shows the ad length or end
+	},
+	onTimeCompleted: '',
+	watchTime: 0,
+	interval: '',
+	pause: function() {
+		ytads.player.pauseVideo();
+	},
+	play: function() {
+		ytads.player.playVideo();
+	},
+	current: 0, // Do not change this
+	init: function(id) {
+		var tag = document.createElement('script');
+		tag.src = "https://www.youtube.com/player_api";
+		var firstScriptTag = document.getElementsByTagName('script')[0];
+		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+		
+		window.onYouTubePlayerAPIReady = function() {
+			ytads.player = new YT.Player(id);
+			delete window.onYouTubePlayerAPIReady;
+		}
+		delete ytads.init;
+	},
+	covered: false, // Do not change this
+	toggleCover: function() { // Please do not use this function as it may change the user experience
+		if (!this.covered) {
+			let el = document.createElement('div');
+			el.id = 'cover';
+			el.style.position = 'absolute';
+			el.style.backgroundColor = 'transparent';
+			window.cover = setInterval(function() {
+				el.style.left = ytads.player.a.offsetLeft + 'px';
+				el.style.top = ytads.player.a.offsetTop + 'px';
+				el.style.width = ytads.player.a.width + 'px';
+				el.style.height = ytads.player.a.height + 'px';
+			});
+			if (this.settings.canMute == true) {
+				let mute = document.createElement('div');
+				mute.attributes.muted = false;
+				mute.onclick = function() {
+					if (this.attributes.muted == false) {
+						this.attributes.muted = true;
+						ytads.player.mute();
+					} else {
+						this.attributes.muted = false;
+						ytads.player.unMute();
+					}
+				}
+				mute.innerHTML = 'mute';
+				el.appendChild(mute);
+			}
+			if (this.settings.timeShows == true) {
+				let time = document.createElement('div');
+				time.id = 'time';
+				time.innerHTML = `0 / ${this.settings.length}`;
+				el.appendChild(time);
+			}
+			document.body.appendChild(el);
+			this.covered = true;
+		} else {
+			document.getElementById('cover').outerHTML = '';
+			clearInterval(cover);
+			delete cover;
+			this.covered = false;
+		}
+	},
+	newVideo: function() {
+		let video;
+		if (this.settings.playType == 'random') {
+			let current = Math.floor(Math.random() * this.videos.length); // Get a random video id
+			video = this.videos[current];
+			if (this.current == this.videos.length - 1 && this.current == current) {
+				current--;
+				video = this.videos[current];
+			} else if (this.current == current && this.current != this.videos.length - 1) {
+				current++;
+				video = this.videos[current];
+			}
+			this.current = current;
+		} else if (this.settings.playType == 'inline') {
+			video = this.videos[this.current]; // Get the next video
+			this.current++; // Add to the current tracker
+			if (this.current == this.videos.length) {
+				this.current = 0;
+			}
+		} else {
+			video = 'rjteBAIPnE0'; // If the type is written incorrectly, use my video and log an error
+			console.error(`The type "${this.settings.playType}" is not valid`);
+			return false;
+		}
+		window.start = video.split(',');
+		video = start[0];
+		window.start = start[1];
+		ytads.interval = setInterval( function() {
+			if (ytads.covered == false) {
+				if (ytads.player.getPlayerState() == 3) {
+					if (ytads.settings.cover == true) {
+						ytads.toggleCover();
+					}
+				}
+			}
+			if (ytads.player.getPlayerState() == 1) {
+				clearInterval(ytads.interval);
+				ytads.player.seekTo(Number(start));
+			}
+		}, 100);
+		window.playint = setInterval( function() {
+			if (ytads.player.getPlayerState() == 1) {
+				if (ytads.watchTime >= ytads.settings.length) {
+					if (typeof(ytads.onTimeCompleted) == 'function') {
+						ytads.onTimeCompleted();
+					}
+					if (ytads.settings.cover == true) {
+						ytads.toggleCover();
+					}
+					ytads.pause();
+					ytads.watchTime = -1;
+					clearInterval(playint);
+					delete playint;
+				}
+				ytads.watchTime++;
+				if (ytads.settings.timeShows == true) {
+					/*let f = ytads.settings.timeFormat;
+					// Check for current
+					let g = f.split('=current=');
+					for (let i = 0; i < g.length; i++) {
+						
+					}*/
+					if (document.getElementById('time') != undefined) {
+						document.getElementById('time').innerHTML = `${ytads.watchTime} / ${ytads.settings.length}`;
+					}
+				}
+			}
+		}, 1000);
+		this.player.cueVideoById({'videoId': video,
+              'suggestedQuality': 'medium'});
+	},
+	player: '',
+}
+console.log('YouTube Advert by DarkHeart Productions is now running, version: 2.0.0');

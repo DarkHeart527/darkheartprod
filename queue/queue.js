1fad2bc04676328a84f93381d3ddf67b2f04bda8
James <DarkHeart527@users.noreply.github.com>
2020-05-20 06:29:09 -0700

    Create queue.js

---
+++ queue/queue.js
@@ -0,0 +1,15 @@
+//app.newTableEntry('beats', ['Timestamp', 'Producer', 'Collaborators', 'Title', 'Status']);
+//app.newTableEntry('songs', ['Timestamp', 'Artist ', 'Title', 'Producer', 'Features', 'Status']);
+
+document.onload = () => {
+
+app.newTableEntry('songs', ['4/5/2020 8:27:07', 'CammieX ', 'ADVICE', 'CammieX', '', 'Pending']);
+app.newTableEntry('songs', ['5/6/2020 18:01:58', 'OffCorpSe', 'Ghost In The Night', 'DarkHeart Productions', '', 'Pending']);
+app.newTableEntry('songs', ['5/6/2020 18:05:17', 'OffCorpSe', 'Crouched In Fear', 'DarkHeart Productions', '', 'Pending']);
+app.newTableEntry('songs', ['5/8/2020 3:18:45', 'D I P', '#BOHEMIAN', 'haku beats', '', 'Pending']);
+app.newTableEntry('songs', ['5/8/2020 3:19:29', 'D I P', '#BOHEMIAN', 'haku beats', '', 'Pending']);
+app.newTableEntry('songs', ['5/8/2020 3:19:30', 'D I P', '#BOHEMIAN', 'haku beats', '', 'Pending']);
+app.newTableEntry('songs', ['5/8/2020 3:19:42', 'D I P', '#BOHEMIAN', 'haku beats', '', 'Pending']);
+app.newTableEntry('songs', ['5/8/2020 3:19:46', 'D I P', '#BOHEMIAN', 'haku beats', '', 'Pending']);
+
+}

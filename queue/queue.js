e652caa6f860424c18f79aeb5ed25cd1e4124d3c
James <DarkHeart527@users.noreply.github.com>
2020-05-20 06:36:23 -0700

    Update queue.js

--- queue/queue.js
+++ queue/queue.js
@@ -4,8 +4,8 @@
 app.newTableEntry('songs', ['4/5/2020 8:27:07', 'CammieX ', 'ADVICE', 'CammieX', '', 'Pending']);
 app.newTableEntry('songs', ['5/6/2020 18:01:58', 'OffCorpSe', 'Ghost In The Night', 'DarkHeart Productions', '', 'Pending']);
 app.newTableEntry('songs', ['5/6/2020 18:05:17', 'OffCorpSe', 'Crouched In Fear', 'DarkHeart Productions', '', 'Pending']);
-app.newTableEntry('songs', ['5/8/2020 3:18:45', 'D I P', '#BOHEMIAN', 'haku beats', '', 'Pending', 'It is not free for beats that are not produced by me']);
-app.newTableEntry('songs', ['5/8/2020 3:19:29', 'D I P', '#BOHEMIAN', 'haku beats', '', 'Pending', 'Duplicate / Spam']);
-app.newTableEntry('songs', ['5/8/2020 3:19:30', 'D I P', '#BOHEMIAN', 'haku beats', '', 'Pending', 'Duplicate / Spam']);
-app.newTableEntry('songs', ['5/8/2020 3:19:42', 'D I P', '#BOHEMIAN', 'haku beats', '', 'Pending', 'Duplicate / Spam']);
-app.newTableEntry('songs', ['5/8/2020 3:19:46', 'D I P', '#BOHEMIAN', 'haku beats', '', 'Pending', 'Duplicate / Spam']);
+app.newTableEntry('songs', ['5/8/2020 3:18:45', 'D I P', '#BOHEMIAN', 'haku beats', '', 'REJECTED', 'It is not free for beats that are not produced by me']);
+app.newTableEntry('songs', ['5/8/2020 3:19:29', 'D I P', '#BOHEMIAN', 'haku beats', '', 'REJECTED', 'Duplicate / Spam']);
+app.newTableEntry('songs', ['5/8/2020 3:19:30', 'D I P', '#BOHEMIAN', 'haku beats', '', 'REJECTED', 'Duplicate / Spam']);
+app.newTableEntry('songs', ['5/8/2020 3:19:42', 'D I P', '#BOHEMIAN', 'haku beats', '', 'REJECTED', 'Duplicate / Spam']);
+app.newTableEntry('songs', ['5/8/2020 3:19:46', 'D I P', '#BOHEMIAN', 'haku beats', '', 'REJECTED', 'Duplicate / Spam']);
